b4eac8390c78a1d04055df01d36ef1baf9de973d
Mikael Weiss <campingmikael@icloud.com>
2025-10-11 15:23:05 -0600

    Initial Commit

---
+++ CareSyncTests/CareSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  CareSyncTests.swift
+//  CareSyncTests
+//
+//  Created by Mikael Weiss on 10/11/25.
+//
+
+import Testing
+@testable import CareSync
+
+struct CareSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
